5c83640b99908d148c06f72a6c0009b8933d3c4a
BookStore <ivansmgo@gmail.com>
2021-04-08 17:41:26 +0200

    Updates

---
+++ precache-manifest.d1236ef0dfe41e8caf3fa20a2174383a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8a5c9f2957c52ba652230b044039fe",
+    "url": "/houseofbooks/index.html"
+  },
+  {
+    "revision": "337f3b20376e36a5535e",
+    "url": "/houseofbooks/static/css/main.ea1e8966.chunk.css"
+  },
+  {
+    "revision": "0973515ffc493280174a",
+    "url": "/houseofbooks/static/js/2.97f21d4e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/houseofbooks/static/js/2.97f21d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "337f3b20376e36a5535e",
+    "url": "/houseofbooks/static/js/main.b613545d.chunk.js"
+  },
+  {
+    "revision": "430a477603e936d012d7",
+    "url": "/houseofbooks/static/js/runtime-main.8be229a6.js"
+  },
+  {
+    "revision": "7966e9a2ec74ed51b54bf6a6a9a268f6",
+    "url": "/houseofbooks/static/media/agataImg.7966e9a2.jpg"
+  },
+  {
+    "revision": "728220a2c1be2703606275bd0c69e23c",
+    "url": "/houseofbooks/static/media/authImage.728220a2.svg"
+  },
+  {
+    "revision": "1bda6e3ff7c8618ad16f3e6a7e8717ff",
+    "url": "/houseofbooks/static/media/cartCorner.1bda6e3f.svg"
+  },
+  {
+    "revision": "684da1c631f95513f5b0be7c00bd6944",
+    "url": "/houseofbooks/static/media/georgeImg.684da1c6.jpg"
+  },
+  {
+    "revision": "2b903a2a586d6d7773c3565011399fee",
+    "url": "/houseofbooks/static/media/header.2b903a2a.svg"
+  },
+  {
+    "revision": "bb2a71649051c03b66ab35d8a113bf12",
+    "url": "/houseofbooks/static/media/jkrImage.bb2a7164.jpg"
+  },
+  {
+    "revision": "c16659031bf1fc0c942b1cdef570e189",
+    "url": "/houseofbooks/static/media/readingBook.c1665903.svg"
+  }
+]);
